013adf2a2579d29ffbb77d6fb2f331a3de9948c1
Mischa Saunders <mischa.saunders@gmail.com>
2017-01-25 17:58:03 +1300

    linted

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -3,11 +3,11 @@ import React from 'react'
 const EIFFEL_TOWER_POSITION = {
   lat: 48.858608,
   lng: 2.294471
-};
+}
 
 const Skilleby = {
   lat: 59.0442751,
-  lng:17.5959056
+  lng: 17.5959056
 }
 
 const TripleHBeef = {
@@ -21,39 +21,39 @@ const TableHurst = {
 
 export default class Map extends React.Component {
 
-    componentDidMount() {
-      this.map = new google.maps.Map(this.refs.map, {
-        center: EIFFEL_TOWER_POSITION,
-        zoom: 1
-      });
+  componentDidMount () {
+    this.map = new google.maps.Map(this.refs.map, {
+      center: EIFFEL_TOWER_POSITION,
+      zoom: 2
+    })
 
-      var marker = new google.maps.Marker({
-          position: Skilleby,
-          map: this.map,
-          title: 'Skilleby!'
-      });
+    var marker = new google.maps.Marker({
+      position: Skilleby,
+      map: this.map,
+      title: 'Skilleby!'
+    })
 
-      var marker = new google.maps.Marker({
-          position: TripleHBeef,
-          map: this.map,
-          title: 'Triple H Beef!'
-      });
-      var marker = new google.maps.Marker({
-          position: TableHurst,
-          map: this.map,
-          title: 'Table Hurst!'
-      });
+    var marker = new google.maps.Marker({
+      position: TripleHBeef,
+      map: this.map,
+      title: 'Triple H Beef!'
+    })
+    var marker = new google.maps.Marker({
+      position: TableHurst,
+      map: this.map,
+      title: 'Table Hurst!'
+    })
+  }
+
+  render () {
+    const mapStyle = {
+      height: 300
     }
 
-    render() {
-      const mapStyle = {
-        height: 300,
-      };
-
-      return (
-          <div ref="map" style={mapStyle}> Loading map...
+    return (
+      <div ref='map' style={mapStyle}> Loading map...
            </div>
-      );
-    }
+    )
+  }
 
 };
