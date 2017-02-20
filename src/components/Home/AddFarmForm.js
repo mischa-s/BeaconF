93516def21e81c220c776ae46da56cfda853b84a
mischa-saunders <mischa.saunders@gmail.com>
2017-02-20 16:08:26 +1300

    new locations are geocoded and added

--- src/components/Home/AddFarmForm.js
+++ src/components/Home/AddFarmForm.js
@@ -3,32 +3,37 @@ import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import { connect } from 'react-redux'
+import FindLatLong from './FindLatLang'
 
 const request = require('superagent')
 
 const SubmitFarm = React.createClass({
   handleSubmit: function (e) {
     e.preventDefault()
-
+    console.log(this.props, 'top of file props')
+    const { dispatch } = this.props
     const farmName = this.refs.farmName.input.value
     const farmLocation = this.refs.farmLocation.input.value
     const farmType = this.refs.farmType.input.value
-    const farmLatitude = this.refs.farmLatitude.input.value
-    const farmLongitude = this.refs.farmLongitude.input.value
     const mainImage = this.refs.mainImage.input.value
-    if (farmName.length > 0) {
-      request.post('api/v1/farms')
-        .send({ name: farmName, location: farmLocation, type: farmType, mainImage: mainImage, longitude: farmLongitude, latitude: farmLatitude })
-        .end((err, data) => {
-        console.log('heard back from api', data.body, err)
-        this.props.dispatch({type: 'GET_ALL_FARMS', payload: data.body})
+
+    if (farmName.length > 0 && farmLocation.length > 0) {
+
+      FindLatLong(farmLocation, function (result) {
+        const longitude = result.Longitude
+        const latitude = result.Latitude
+        console.log(longitude, latitude, 'geocodeAddress result from submit')
+
+        request.post('api/v1/farms')
+          .send({ name: farmName, location: farmLocation, type: farmType, mainImage: mainImage, latitude, longitude})
+          .end((err, data) => {
+          dispatch({type: 'GET_ALL_FARMS', payload: data.body})
+        })
       })
 
       this.refs.farmName.input.value = ''
       this.refs.farmLocation.input.value = ''
       this.refs.farmType.input.value = ''
-      this.refs.farmLocation.input.value = ''
-      this.refs.farmLongitude.input.value = ''
       this.refs.mainImage.input.value = ''
     } else {
       this.refs.farmName.focus()
@@ -40,8 +45,6 @@ const SubmitFarm = React.createClass({
         <form>
           <Paper className='paper' zDepth={3}>
             <TextField hintText='Farm Name' className='input' ref='farmName' fullWidth={true} />
-            <br />
-            <TextField hintText='Farm Location' className='input' ref='farmLocation' fullWidth={true} />
             <br />
             <TextField hintText='Farm Location' className='input' ref='farmLocation' fullWidth={true} />
             <br />
--- src/components/Home/GoogleMap.js
+++ src/components/Home/GoogleMap.js
@@ -1,5 +1,4 @@
 const React = require('react')
-
 const EIFFEL_TOWER_POSITION = {
   lat: 48.858608,
   lng: 2.294471
@@ -23,22 +22,7 @@ class Map extends React.Component {
         title: farm.name
       })
     })
-    this.FindLatLong(forGeocoding, function (result) {
-      console.log(result, 'geocodeAddress result')
-    })
     return arrOfFarms
-  }
-
-  FindLatLong (address, callback) {
-    var geocoder = new google.maps.Geocoder()
-    geocoder.geocode({ 'address': address }, function (results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        console.log(results[0].geometry.location, 'result?')
-        var lat = results[0].geometry.location.lat()
-        var lng = results[0].geometry.location.lng()
-        callback({ Status: "OK", Latitude: lat, Longitude: lng })
-      }
-    })
   }
 
   render () {
@@ -54,28 +38,3 @@ class Map extends React.Component {
 
 }
 module.exports = (Map)
-
-// // This event listener will call addMarker() when the map is clicked.
-// map.addListener('click', function(event) {
-//   addMarker(event.latLng);
-// });
-
-// Adds a marker to the map and push to the array.
-// function addMarker(location) {
-//         var marker = new google.maps.Marker({
-//           position: location,
-//           map: map
-//         });
-//         markers.push(marker);
-//       }
-
-// Sets the map on all markers in the array.
-//       function setMapOnAll(map) {
-//         for (var i = 0; i < markers.length; i++) {
-//           markers[i].setMap(map);
-//         }
-//       }
-// Shows any markers currently in the array.
-// function showMarkers() {
-//   setMapOnAll(map);
-// }
